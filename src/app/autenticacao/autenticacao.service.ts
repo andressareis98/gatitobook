9f4e0c4e4ef6072753ac33e6f1135cfa007c8980
Andressa Reis Araujo <andressareis98@outlook.com>
2021-07-06 21:00:02 -0300

    feat: edit autentication

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,17 +1,33 @@
+import { UsuarioService } from './usuario/usuario.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AutenticacaoService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
-  autentica(usuario: string, senha: string): Observable<any> {
-    return this.httpClient.post('http://localhost:3000/user/login', {
-      userName: usuario,
-      password: senha,
-    });
+  autentica(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.httpClient
+      .post(
+        'http://localhost:3000/user/login',
+        {
+          userName: usuario,
+          password: senha,
+        },
+        { observe: 'response' } // Serve para que tenhamos acesso ao header da requisição
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.headers.get('x-access-token') ?? ''; // Capturando o token que vem no header
+          this.usuarioService.salvaToken(authToken); // Salvando o token
+        })
+      );
   }
 }
