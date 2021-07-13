fb9ceb32f337b3ac63d4f16a8ca3ecd567828ccd
Andressa Reis Araujo <andressareis98@outlook.com>
2021-07-13 11:19:52 -0300

    feat: service list of animals

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -1,17 +1,14 @@
+import { CartaoModule } from './../componentes/cartao/cartao.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListaDeAnimaisComponent } from './lista-de-animais/lista-de-animais.component';
-
+import { AnimalComponent } from './animal/animal.component';
+import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 
 @NgModule({
-  declarations: [
-    ListaDeAnimaisComponent
-  ],
-  imports: [
-    CommonModule,
-    AnimaisRoutingModule
-  ]
+  declarations: [ListaDeAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent],
+  imports: [CommonModule, AnimaisRoutingModule, CartaoModule],
 })
-export class AnimaisModule { }
+export class AnimaisModule {}
